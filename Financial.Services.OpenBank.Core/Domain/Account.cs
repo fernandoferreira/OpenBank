13174cd2fe5b558365ba306447e81ed6a84eebcb
fernandofereira <ferreira.sf@gmail.com>
2022-12-25 11:09:02 -0300

    Added main properties to account class.

--- Financial.Services.OpenBank.Core/Domain/Account.cs
+++ Financial.Services.OpenBank.Core/Domain/Account.cs
@@ -3,8 +3,18 @@ namespace Financial.Services.OpenBank.Core.Domain
 {
     public class Account
     {
+        private Guid id;
+        private Guid userId;
+        private decimal amount;
+
         public Account()
         {
         }
+
+        public Guid Id { get => id; set => id = value; }
+
+        public Guid UserId { get => userId; set => userId = value; }
+
+        public decimal Amount { get => amount; private set => amount = value; }
     }
 }
